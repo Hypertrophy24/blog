54861e7889ef72a45c7e031e73e2c08070d83e80
Hypertrophy24 <brown79liam@gmail.com>
2024-10-08 21:41:50 -0600

    second commit

---
+++ accounts/views.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+from django.views.generic import CreateView
+
+# Create your views here.
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "signup.html"
+
+class LogInView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+    template_name = "login.html"
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,35 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.views.generic.edit import CreateView
+from django.views.generic import ListView, DetailView, UpdateView, DeleteView
 # Create your views here.
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, "home.html", {'posts':posts})
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+    context_object_name = 'post_list'
 
-def post_detail(request,pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request,"post_detail.html",{"post":post})
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title","author", "body"]
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+
+class BlogDeleteView(DeleteView):
+    
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
+
+
+    
